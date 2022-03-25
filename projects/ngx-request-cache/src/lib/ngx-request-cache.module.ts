164bc10f5d19345a610cc7c6c93927a913785160
Daniel Delgado <dedd1993@gmail.com>
2022-03-25 00:14:30 -0500

    feat: use module instead import service and interceptor manually

--- projects/ngx-request-cache/src/lib/ngx-request-cache.module.ts
+++ projects/ngx-request-cache/src/lib/ngx-request-cache.module.ts
@@ -1,8 +1,16 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+
+import { RequestCacheInterceptor } from './ngx-request-cache.interceptor';
+import { RequestCacheService } from './ngx-request-cache.service';
 
 @NgModule({
   declarations: [],
   imports: [
+  ],
+  providers: [
+    RequestCacheService,
+    { provide: HTTP_INTERCEPTORS, useClass: RequestCacheInterceptor, multi: true },
   ],
   exports: []
 })
